c457e50bd064f099f845c5845223a474170b68ac
rahiggin <beccahigginbotham@gmail.com>
2023-03-13 17:48:53 -0500

    added /home for dashboard on app vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -38,7 +38,7 @@ export default {
           <!-- <nav class="mt-10"> -->
           <ul class="flex flex-col gap-4">
             <li>
-              <router-link to="/">
+              <router-link to="/home">
                 <span
                   style="position: relative; top: 6px"
                   class="material-icons"
