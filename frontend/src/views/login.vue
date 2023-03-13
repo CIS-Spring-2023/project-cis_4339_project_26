d4bb79d99d5b371f52e11700018e45a6dadb8d05
rahiggin <beccahigginbotham@gmail.com>
2023-03-12 22:05:07 -0500

    fixed the style to tailwind for login

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -3,20 +3,23 @@
 <template>
     <main>
         <div>
-            <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">
+            <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10 mb-10">
                 Login
             </h1>
         </div>
-        <form @submit.prevent="store.login(username, password)" novalidate="true">
+        <form class="flex flex-col justify-center items-center" @submit.prevent="store.login(username, password)" novalidate="true">
 
-        <div class="form-group text-center">
+        <div class="text-center w-80">
             <label>User Name</label>
-            <input type="text" class="form-control" v-model="username" placeholder="username" required>
-        <div class="form-group">
+            <input type="text" 
+            class="mt-2 mb-5 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" v-model="username" placeholder="username" required>
+        <div class="text-center" >
             <label>Password</label>
-            <input type="password" class="form-control" v-model="password" placeholder="password" required>
+            <input type="password" 
+            class="mt-2 mb-5 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" 
+            v-model="password" placeholder="password" required>
             </div>
-            <button class="btn btn-danger mt-3">Submit</button>
+            <button class="bg-red-700 text-white rounded">Submit</button>
         </div>
         </form>
     </main>
@@ -36,6 +39,12 @@ export default {
         const store = useLoggedInUserStore()
         return {
             store,
+        }
+    },
+    data() {
+        return {
+            isAuthorized: false,
+            restrictPages: ['/eventform', '/intakeform']
         }
     }
 }
