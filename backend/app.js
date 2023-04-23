d26e2bb4fa3cf4fb0fa825ddaae5bb4b96ce5ba4
rahiggin <beccahigginbotham@gmail.com>
2023-04-23 17:07:27 -0500

    added users and services to app.js

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use('/clients', require('./routes/clients'))
 app.use('/events', require('./routes/events'))
 app.use('/org', require('./routes/org'))
 app.use('/services', require('./routes/services'))
-
+app.use('/users', require('/.routes/users'))
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
