2810e91405f8cb8a9e61c30d985f6921d21b7f29
JChacon0222 <89750587+JChacon0222@users.noreply.github.com>
2023-04-26 01:37:13 -0500

    sprint 3 for charts
    working on charts...trying to connect them to the db

--- frontend/src/components/barChart2.vue
+++ frontend/src/components/barChart2.vue
@@ -1,8 +1,9 @@
 <script>
 import { Chart, registerables } from 'chart.js'
+// import { MongoClient } from 'mongodb' // import the MongoClient from the mongodb library
 Chart.register(...registerables)
 
-//Event Name+Event Date, Number of Attendees 
+//Zip Codes 
 export default {
   props: {
     label: {
@@ -17,16 +18,28 @@ export default {
     const borderColor = backgroundColor.map((e) =>
       e.replace(/[\d\.]+\)$/g, '1)')
     )
-    await new Chart(this.$refs.attendanceChart, {
-      type: 'bar',
+
+    // establish a connection to the MongoDB database
+    const client = new MongoClient('mongodb://localhost:3000')
+    await client.connect()
+    const db = client.db('mydatabase')
+
+    // query for zip codes and their associated data
+    const collection = db.collection('test')
+    const zipCodes = await collection.find().toArray()
+    const zipCodeLabels = zipCodes.map((zipCode) => zipCode.code)
+    const zipCodeData = zipCodes.map((zipCode) => zipCode.data)
+
+    await new Chart(this.$refs.attendanceChart2, {
+      type: 'pie',
       data: {
-        labels: this.label,
+        labels: zipCodeLabels,
         datasets: [
           {
             borderWidth: 1,
             backgroundColor: backgroundColor,
             borderColor: borderColor,
-            data: this.chartData
+            data: zipCodeData
           }
         ]
       },
@@ -52,6 +65,7 @@ export default {
         maintainAspectRatio: true
       }
     })
+    await client.close() // close the database connection
   },
   methods: {
     getColor() {
@@ -63,6 +77,6 @@ export default {
 </script>
 <template>
   <div class="shadow-lg rounded-lg overflow-hidden">
-    <canvas class="p-10" ref="attendanceChart"></canvas>
+    <canvas class="p-10" ref="attendanceChart2"></canvas>
   </div>
 </template>
--- frontend/src/components/homePage.vue
+++ frontend/src/components/homePage.vue
@@ -1,44 +1,138 @@
 <script>
 import { DateTime } from 'luxon'
 import axios from 'axios'
+import AttendanceChart from './barChart.vue'
+import AttendanceChart2 from './barChart2.vue'
 const apiURL = import.meta.env.VITE_ROOT_API
-import Chart from 'chart.js/auto'
 
 export default {
+  components: {
+    AttendanceChart,
+    AttendanceChart2
+  },
+  data() {
+    return {
+      recentEvents: [],
+      labels: [],
+      chartData: [],
+      loading: false,
+      error: null
+    }
+  },
   mounted() {
-    const ctx = document.getElementById('myChart')
-
-    const myChart = new Chart(ctx, {
-      type: 'doughnut',
-      data: {
-        labels: ['77494 ', '77449 ', '77407 ', '77386 ', '77433 ', '77099 '],
-        datasets: [
-          {
-            label: '# of Ppl in Zip Code ',
-            data: [4, 19, 21, 29, 23, 17],
-            backgroundColor: [
-              'rgba(255, 0, 0, 0.7)',
-              'rgba(0, 255, 0, 0.7)',
-              'rgba(255, 255, 0, 0.7)',
-              'rgba(255, 0, 255, 0.7)',
-              'rgba(0, 255, 255, 0.7)',
-              'rgba(0, 0, 255, 0.7)'
-            ],
-            borderColor: [
-              'rgba(0, 0, 0, 1)',
-              'rgba(0, 0, 0, 1)',
-              'rgba(0, 0, 0, 1)',
-              'rgba(0, 0, 0, 1)',
-              'rgba(0, 0, 0, 1)',
-              'rgba(0, 0, 0, 1)'
-            ],
-            borderWidth: .25
+    this.getAttendanceData()
+  },
+  methods: {
+    async getAttendanceData() {
+      try {
+        this.error = null
+        this.loading = true
+        const response = await axios.get(`${apiURL}/events/attendance`)
+        this.recentEvents = response.data
+        this.labels = response.data.map(
+          (item) => `${item.name} (${this.formattedDate(item.date)})`
+        )
+        this.chartData = response.data.map((item) => item.attendees.length)
+      } catch (err) {
+        if (err.response) {
+          // client received an error response (5xx, 4xx)
+          this.error = {
+            title: 'Server Response',
+            message: err.message
           }
-        ]
+        } else if (err.request) {
+          // client never received a response, or request never left
+          this.error = {
+            title: 'Unable to Reach Server',
+            message: err.message
+          }
+        } else {
+          // There's probably an error in your code
+          this.error = {
+            title: 'Application Error',
+            message: err.message
+          }
+        }
+      }
+      this.loading = false
+    },
+    formattedDate(datetimeDB) {
+      const dt = DateTime.fromISO(datetimeDB, {
+        zone: 'utc'
+      })
+      return dt
+        .setZone(DateTime.now().zoneName, { keepLocalTime: true })
+        .toLocaleString()
+    },
+    // method to allow click through table to event details
+    editEvent(eventID) {
+      this.$router.push({ name: 'eventdetails', params: { id: eventID } })
+    }
+  },
+
+  data() {
+      return {
+        recentEvents: [],
+        labels: [],
+        chartData: [],
+        loading: false,
+        error: null
+      }
+    },
+    mounted() {
+      this.getZipcodeData()
+    },
+    methods: {
+      async getZipcodeData() {
+        try {
+          this.error = null
+          this.loading = true
+          const response = await axios.get(`${apiURL}/client/zip`)
+          this.recentEvents = response.data
+          this.labels = response.data.map(
+          // const dataVerified = verify.then((res)=>res.data)
+          // return dataVerified
+
+            (item) => `${item.name} (${this.formattedDate(item.date)})`
+          )
+          this.chartData = response.data.map((item) => item.attendees.length)
+        } catch (err) {
+          if (err.response) {
+            // client received an error response (5xx, 4xx)
+            this.error = {
+              title: 'Server Response',
+              message: err.message
+            }
+          } else if (err.request) {
+            // client never received a response, or request never left
+            this.error = {
+              title: 'Unable to Reach Server',
+              message: err.message
+            }
+          } else {
+            // There's probably an error in your code
+            this.error = {
+              title: 'Application Error',
+              message: err.message
+            }
+          }
+        }
+        this.loading = false
       },
-    })
+      formattedDate(datetimeDB) {
+        const dt = DateTime.fromISO(datetimeDB, {
+          zone: 'utc'
+        })
+        return dt
+          .setZone(DateTime.now().zoneName, { keepLocalTime: true })
+          .toLocaleString()
+      },
+      // method to allow click through table to event details
+      editEvent(eventID) {
+        this.$router.push({ name: 'eventdetails', params: { id: eventID } })
+      }
+    }
   }
-}
 </script>
 
 <template>
@@ -74,39 +168,39 @@ export default {
                 <td class="p-2 text-left">{{ event.attendees.length }}</td>
               </tr>
             </tbody>
-
-            <tbody class="divide-y divide-gray-300">
-              <tr>
-                <td class="p-2 text-left">Event 1</td>
-                <td class="p-2 text-left">3/3/2023</td>
-                <td class="p-2 text-left">3</td>
-              </tr>
-              <tr>
-                <td class="p-2 text-left">Event 2</td>
-                <td class="p-2 text-left">3/25/2023</td>
-                <td class="p-2 text-left">5</td>
-              </tr>
-              <tr>
-                <td class="p-2 text-left">Event 3</td>
-                <td class="p-2 text-left">4/7/2023</td>
-                <td class="p-2 text-left">7</td>
-              </tr>
-              <tr>
-                <td class="p-2 text-left">Event 4</td>
-                <td class="p-2 text-left">4/18/2023</td>
-                <td class="p-2 text-left">6</td>
-              </tr>
-              <tr>
-                <td class="p-2 text-left">Event 5</td>
-                <td class="p-2 text-left">4/30/2023</td>
-                <td class="p-2 text-left">4</td>
-              </tr>
-            </tbody>
           </table>
           <div>
-            <br />
-            <canvas id="myChart" width="20" height="20"></canvas>
-            <br /><br />
+            <AttendanceChart
+              v-if="!loading && !error"
+              :label="labels"
+              :chart-data="chartData"
+            ></AttendanceChart>
+            <!-- Start of loading animation -->
+            <div class="mt-40" v-if="loading">
+              <p
+                class="text-6xl font-bold text-center text-gray-500 animate-pulse"
+              >
+                Loading...
+              </p>
+            </div>
+            <!-- End of loading animation -->
+            <!-- Start of error alert -->
+            <div class="mt-12 bg-red-50" v-if="error">
+              <h3 class="px-4 py-1 text-4xl font-bold text-white bg-red-800">
+                {{ error.title }}
+              </h3>
+              <p class="p-4 text-lg font-bold text-red-900">
+                {{ error.message }}
+              </p>
+            </div>
+            <!-- End of error alert -->
+          </div>
+          <div>
+            <AttendanceChart2
+              v-if="!loading && !error"
+              :label="labels"
+              :chart-data="chartData"
+            ></AttendanceChart2>
             <!-- Start of loading animation -->
             <div class="mt-40" v-if="loading">
               <p
@@ -128,6 +222,7 @@ export default {
             </div>
             <!-- End of error alert -->
           </div>
+
         </div>
       </div>
     </div>
